e6c9170ba6d3daa8c0680db686e9578435bbaab3
Jose Hurtado <hello@hurtadojose.com>
2022-07-01 01:17:30 +0000

    Exercises 1 and 2 Done

--- .learn/resets/02-What-is-a-function/app.js
+++ .learn/resets/02-What-is-a-function/app.js
@@ -1,5 +1,3 @@
-// Code Already Compiled
-
 function sum(number1,number2) {
     return number1 + number2;
 }
@@ -8,13 +6,7 @@ var total = sum(2,3);
 var total2 = sum(5,10);
 var total3 = sum(2,3);
 
+
 console.log(total);
 console.log(total2);
 console.log(total3);
-
-number1 = 3445324;
-number2 = 53454423;
-
-let superduper = number1 + number2;
-
-console.log(superduper);
