f00ae58a3c0f0313558f468a0101dee02992e603
Jose Hurtado <hello@hurtadojose.com>
2022-07-01 04:56:45 +0000

    WIP Exercise 10

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,12 +1,15 @@
-const rapid = (myString) => {
-    let consonants = [];
-    for(let letter of myString){
-        if(['a','e','i','o','u'].includes(letter) == false)
-            consonants.push(letter)
+let rapid = (string) => {
+    let vowels = ['a', 'e', 'i', 'o', 'u',
+      'A', 'E', 'I', 'O', 'U'];
+    let result = "";
+    for (let i = 0; i < string.length; i++) {
+      if (!vowels.includes(string[i])) {
+        result += string[i];
+      }
     }
-    return consonants.join('');
-};
-
-// Work above this line; do not change code below
-let str = "John";
-console.log(rapid(str));
+    return result.toUpperCase();
+  }
+  
+  // Work above this line; do not change code below
+  let str = "John";
+  console.log(rapid(str));  
