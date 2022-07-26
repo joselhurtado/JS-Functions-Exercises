a7e0d0510cb2469ed774b15f718c90724431c510
Jose Hurtado <hello@hurtadojose.com>
2022-07-26 04:54:46 +0000

    Exercise 6 Already Compiled

--- exercises/06-Arrow-function/app.js
+++ exercises/06-Arrow-function/app.js
@@ -1,13 +1,6 @@
-var dollarToEuro = function(dollarValue){
-	return dollarValue * 0.89;
-}
-var euroToYen = function(euroValue){
-	return euroValue * 124.15;
-}
-//***** YOUR CODE BELOW ↓ ******///
+// Code Already Compiled 
 
-const multy = (a,b) => {
-    return a*b;
-}
+const multy = (a,b) => a*b;
 
-console.log(dollarToEuro(1, 1) + euroToYen(1, 1) * 137)
+//Dont edit anything below this line
+console.log(multy(324234,47))
