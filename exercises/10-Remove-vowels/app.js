6d1db76b71b17ed68b52ee34526feafc07f05eca
Jose Hurtado <hello@hurtadojose.com>
2022-07-26 04:43:27 +0000

    Exercise 9 Compiled with some errors

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,15 +1,16 @@
-let rapid = (string) => {
-    let vowels = ['a', 'e', 'i', 'o', 'u',
-      'A', 'E', 'I', 'O', 'U'];
-    let result = "";
-    for (let i = 0; i < string.length; i++) {
-      if (!vowels.includes(string[i])) {
-        result += string[i];
-      }
+//Code Already Compiled with Errors
+
+const rapid = (str) => {
+  let newString = "";
+  let i;
+  for (i = 0; i < str.length; i++) {
+    if(str.charAt(i) != "o") {
+      newString += str.charAt(i);
     }
-    return result.toUpperCase();
-  }
-  
-  // Work above this line; do not change code below
-  let str = "John";
-  console.log(rapid(str));  
+}
+  return newString.toUpperCase();
+}
+
+// Work above this line; do not change code below
+let str = "John";
+console.log(rapid(str));  
